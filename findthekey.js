254ff6bbb1cfd2c46e2cde8c428d114c19774507
MrBubbltx <94663195+MrBubbltx@users.noreply.github.com>
2022-04-19 11:58:57 +0800

    Newlink

--- findthekey.js
+++ findthekey.js
@@ -6,7 +6,7 @@ let openkeys = document.querySelector('#openkeys');
 
 function findthekey(){
     let ricklink = "https://www.youtube.com/watch?v=fcZXfoB2f70";
-
+    let bingchillinlink = "https://youtu.be/6AY-NcO7DOA";
 
     if(userinput.value == "D44gIjerKKwQ"){
         progresstrigger.classList.add("load");
@@ -17,6 +17,15 @@ function findthekey(){
             progresstrigger.classList.remove("load");
         }, 1000);
 
+    }
+    else if(userinput.value == "BiChln_9402Dam"){
+        progresstrigger.classList.add("load");
+        setTimeout(function(){
+            typenlinked.style.display = "flex";
+            userinput.value = "";
+            followlink.setAttribute("href", bingchillinlink);
+            progresstrigger.classList.remove("load");
+        }, 1000);
     }
     else{
         alert("Hmmmm... Seems like your key is Invalid.");
