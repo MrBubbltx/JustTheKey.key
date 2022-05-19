1bea40c3b23e6ef43d7f37ed8b4a09d7f50f8f5d
MrBubbltx <94663195+MrBubbltx@users.noreply.github.com>
2022-05-19 14:53:01 +0800

    Update findthekey.js

--- findthekey.js
+++ findthekey.js
@@ -7,6 +7,7 @@ let openkeys = document.querySelector('#openkeys');
 function findthekey(){
     let ricklink = "https://www.youtube.com/watch?v=fcZXfoB2f70";
     let bingchillinlink = "https://youtu.be/6AY-NcO7DOA";
+    let dreamsusremix = "https://www.youtube.com/watch?v=NkFrnFooMgk";
 
     if(userinput.value == "D44gIjerKKwQ"){
         progresstrigger.classList.add("load");
@@ -27,6 +28,15 @@ function findthekey(){
             progresstrigger.classList.remove("load");
         }, 1000);
     }
+    else if(userinput.value == "DZA1pvKT"){
+        progresstrigger.classList.add("load");
+        setTimeout(function(){
+            typenlinked.style.display = "flex";
+            userinput.value = "";
+            followlink.setAttribute("href", dreamsusremix);
+            progresstrigger.classList.remove("load");
+        }, 1000);
+    }
     else{
         alert("Hmmmm... Seems like your key is Invalid.");
     }
@@ -34,4 +44,33 @@ function findthekey(){
 
 function scrollviewinput(){
     openkeys.scrollIntoView({behavior: "smooth"});
+}
+
+let developer = 0;
+let debounce = 0;
+
+document.addEventListener("contextmenu", (e) => {
+    if(developer == 0){
+        e.preventDefault();
+    }
+})
+
+document.onkeydown = function(e) {
+    if(developer == 0){
+        if(event.keyCode == 123) {
+            return false;
+         }
+         if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+            return false;
+         }
+         if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+            return false;
+         }
+         if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+            return false;
+         }
+         if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+            return false;
+         }
+    }
 }
